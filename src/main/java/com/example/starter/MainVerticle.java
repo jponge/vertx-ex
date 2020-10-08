9369f6f605a40aabde8050051e435f458f95f58c
Julien Ponge <jponge@redhat.com>
2020-10-08 22:54:54 +0200

    Initial import

---
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -0,0 +1,23 @@
+package com.example.starter;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("Content-Type", "text/plain")
+        .end("Hello from Vert.x!");
+    }).listen(8080, http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("HTTP server started on port 8080");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    });
+  }
+}
